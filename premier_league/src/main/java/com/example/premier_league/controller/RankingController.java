bec388d3d13b293d827af9688ebd8b0c1e9930a6
VanThanh34 <vvt1304@gmail.com>
2025-11-19 16:00:46 +0700

    update BXH

--- premier_league/src/main/java/com/example/premier_league/controller/RankingController.java
+++ premier_league/src/main/java/com/example/premier_league/controller/RankingController.java
@@ -1,22 +1,22 @@
-package com.example.premier_league.controller;
-
-import com.example.premier_league.service.IRankingService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-@RequiredArgsConstructor
-public class RankingController {
-
-    private final IRankingService rankingService;
-
-    @GetMapping("/ranking")
-    public String showRankingPage(Model model) {
-
-        model.addAttribute("rankings", rankingService.getRanking());
-
-        return "ranking";
-    }
-}
+//package com.example.premier_league.controller;
+//
+//import com.example.premier_league.service.IRankingService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//@RequiredArgsConstructor
+//public class RankingController {
+//
+//    private final IRankingService rankingService;
+//
+//    @GetMapping("/ranking")
+//    public String showRankingPage(Model model) {
+//
+//        model.addAttribute("rankings", rankingService.getRanking());
+//
+//        return "ranking";
+//    }
+//}
