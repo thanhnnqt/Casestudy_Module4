7c53d4a807a161396fb251503eb7cbe9c6b19429
VanThanh34 <vvt1304@gmail.com>
2025-11-20 10:02:30 +0700

    update thống kê trận đấu và diễn biến chính phần admin

--- premier_league/src/main/java/com/example/premier_league/controller/MatchStatsController.java
+++ premier_league/src/main/java/com/example/premier_league/controller/MatchStatsController.java
@@ -4,18 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/admin/stats")
 @RequiredArgsConstructor
 public class MatchStatsController {
 
-    @GetMapping("/admin/stats/{matchId}")
-    public String updateStatsPage(
-            @PathVariable Long matchId,
-            Model model
-    ) {
-        model.addAttribute("matchId", matchId);
+    @GetMapping
+    public String formUpdateStats(){
         return "admin_stats";
     }
 }
