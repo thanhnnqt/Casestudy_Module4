013590e82b7bbd8918a492c2b8b754be329ec254
VanThanh34 <vvt1304@gmail.com>
2025-11-20 10:45:43 +0700

    update lấy matchId

--- premier_league/src/main/java/com/example/premier_league/controller/MatchEventController.java
+++ premier_league/src/main/java/com/example/premier_league/controller/MatchEventController.java
@@ -1,14 +1,19 @@
 package com.example.premier_league.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MatchEventController {
 
     @GetMapping("/admin/events")
-    public String adminEventsPage() {
+    public String adminEventsPage(@RequestParam Long matchId, Model model) {
+        model.addAttribute("matchId", matchId);
         return "admin_event";
     }
+
+
 
 }
